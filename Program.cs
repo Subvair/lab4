340514ca6f15658174eadb11456b70fbf3f98104
subvair <vadimogienko42k@gmail.com>
2025-03-28 18:04:51 +0700

    Добавил методы бинарной и

--- Program.cs
+++ Program.cs
@@ -11,12 +11,38 @@ public class TextFile {
         Content = content;
     }
 
+    public void SaveBinary(string path) {
+        using (FileStream fs = new FileStream(path, FileMode.Create)) {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, this);
+        }
+    }
+
+    public static TextFile LoadBinary(string path) {
+        using (FileStream fs = new FileStream(path, FileMode.Open)) {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (TextFile)formatter.Deserialize(fs);
+        }
+    }
+
+    public void SaveXml(string path) {
+        using (FileStream fs = new FileStream(path, FileMode.Create)) {
+            XmlSerializer serializer = new XmlSerializer(typeof(TextFile));
+            serializer.Serialize(fs, this);
+        }
+    }
+
+    public static TextFile LoadXml(string path) {
+        using (FileStream fs = new FileStream(path, FileMode.Open)) {
+            XmlSerializer serializer = new XmlSerializer(typeof(TextFile));
+            return (TextFile)serializer.Deserialize(fs);
+        }
+    }
     
 }
 
 
-class Program
-{
+class Program {
     static void Main() {
 
     }
