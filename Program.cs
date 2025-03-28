d1124885e2c8796c335c3865252cb6643919ac6a
subvair <vadimogienko42k@gmail.com>
2025-03-28 17:33:48 +0700

    Создал класс для текстового файла

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿using System;
 
+[Serializable]
+public class TextFile {
+    public string FileName { get; set; }
+    public string Content { get; set; }
+
+    public TextFile() { }
+    public TextFile(string fileName, string content) {
+        FileName = fileName;
+        Content = content;
+    }
+
+    
+}
+
+
 class Program
 {
     static void Main() {
